7b6bc53c583d39b9f7348449bbc5439087d29530
eromnoj <j.moreschi@outlook.fr>
2022-06-15 14:11:59 +0200

    Add swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({summary: 'return hello world'})
+  @ApiResponse({status: 403, description: 'Forbidden.'})
+  @ApiResponse({status: 200, description: 'Authorized'})
   getHello(): string {
     return this.appService.getHello();
   }
